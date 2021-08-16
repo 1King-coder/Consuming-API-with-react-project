0da684306fd2db15e471d0505c3edc7c719ee9fa
1King-coder <progvitor@gmail.com>
2021-08-16 20:56:07 -0300

    Requesting and showing Students from API

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
-// import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { get } from 'lodash';
+import { Link } from 'react-router-dom';
+import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import { Container } from '../../styles/GlobalStyles';
-// import { Paragraf } from './styled';
-// import * as exampleActions from '../../store/modules/example/actions';
+import { StudentContainer, ProfilePicture } from './styled';
+import axios from '../../services/axios';
 
 export default function Students() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/students');
+
+      console.log(response.data);
+      setStudents(response.data);
+    }
+    getData();
+  }, []);
+
   return (
     <Container>
       <h1>Students</h1>
+      <StudentContainer>
+        {students.map((student) => (
+          <div key={String(student.id)}>
+            <ProfilePicture>
+              {get(student, 'Files[0].url', false) ? (
+                <a href={student.Files[0].url} target="_blank" rel="noreferrer">
+                  <img src={student.Files[0].url} alt="" />
+                </a>
+              ) : (
+                <FaUserCircle size={36} />
+              )}
+            </ProfilePicture>
+            <span>{student.nome}</span>
+            <span>{student.email}</span>
+
+            <Link to={`/student/${student.id}/edit`}>
+              <FaEdit size={16} />
+            </Link>
+            <Link to={`/student/${student.id}/delete`}>
+              <FaWindowClose size={16} />
+            </Link>
+          </div>
+        ))}
+      </StudentContainer>
     </Container>
   );
 }
--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'URL_HERE',
+  baseURL: 'http://54.232.12.74',
 });
