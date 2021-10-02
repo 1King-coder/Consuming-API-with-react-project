e83d7ade14e0957a6e1f50575012a5c682238695
1King-coder <progvitor@gmail.com>
2021-10-01 22:19:16 -0300

    Implemented File uploading functionality finishing the project

--- src/pages/Photos/index.js
+++ src/pages/Photos/index.js
@@ -1,14 +1,98 @@
-import React from 'react';
-// import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { get } from 'lodash';
 
+import { toast } from 'react-toastify';
+import Loading from '../../components/Loading';
 import { Container } from '../../styles/GlobalStyles';
-// import { Paragraf } from './styled';
-// import * as exampleActions from '../../store/modules/example/actions';
+import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
 
-export default function Photos() {
+export default function Photos({ match }) {
+  const id = get(match, 'params.id', '');
+
+  const dispatch = useDispatch();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [photo, setPhoto] = useState('');
+  const [file, setFile] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+
+        const { data } = await axios.get(`/students/${id}`);
+        setPhoto(get(data, 'Files[0].url', ''));
+
+        setIsLoading(false);
+      } catch {
+        setIsLoading(false);
+
+        toast.error('An error has ocurred while trying to get image.');
+        history.push('/');
+      }
+    };
+    getData();
+  }, [id]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const sentFile = e.target.files[0];
+    const fileURL = URL.createObjectURL(sentFile);
+
+    setPhoto(fileURL);
+    setFile(sentFile);
+  };
+
+  const handleFileUpload = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('student_id', id);
+    formData.append('file', file);
+
+    try {
+      setIsLoading(true);
+
+      await axios.post('/upload/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      toast.success('Photo successfuly uploaded!');
+
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+
+      const status = get(err, 'response.status', 0);
+      toast.error('An error has ocurred while trying to upload the file.');
+
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
   return (
     <Container>
-      <h1>Photos</h1>
+      <Loading isLoading={isLoading} />
+
+      <Title>Photos</Title>
+
+      <Form onSubmit={handleFileUpload}>
+        <label htmlFor="photo">
+          {photo ? <img src={photo} alt="file" /> : 'Select'}
+          <input type="file" id="photo" onChange={handleChange} />
+        </label>
+        <button type="submit">Send photo</button>
+      </Form>
     </Container>
   );
 }
+
+Photos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
--- src/pages/Photos/styled.js
+++ src/pages/Photos/styled.js
@@ -1,3 +1,36 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragraf = styled.p``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  label {
+    width: 180px;
+    height: 180px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #eee;
+    border: 5px dashed ${colors.primaryColor};
+    border-radius: 50%;
+    margin: 30px auto;
+    cursor: pointer;
+    overflow: hidden;
+  }
+
+  img {
+    width: 180px;
+    height: 180px;
+  }
+
+  input {
+    display: none;
+  }
+
+  button {
+    margin: 10px auto;
+  }
+`;
+export const Title = styled.h1`
+  text-align: center;
+`;
